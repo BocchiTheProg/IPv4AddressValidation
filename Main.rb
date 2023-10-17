32510e7204def32e2c170af6514c6e242e2df83a
BocchiTheProg <bocchitheprog@gmail.com>
2023-10-17 16:55:42 +0300

    Created method with only use one regular expression to validate IPv4.

--- Main.rb
+++ Main.rb
@@ -1,1 +1,15 @@
+def ipv4_address_no_regex?(str)
+  blocks = str.split(".")
+  return false unless blocks.size == 4
+  return false unless blocks.all? { |block| block.size <=3 }
+  return false unless blocks.all? { |block| block.match? /^\d+$/ }
+  return false unless blocks.all? { |block| block.to_i.between?(0, 255)}
+  blocks.each do |block|
+    return false if block.size == 3 and block[0].to_i == 0
+    return false if block.size == 2 and block[0].to_i == 0
+  end
+  true
+end
 puts "Hello IPv4 Address validation"
+str = "12.222.3.10"
+puts "Result of method without regex: #{ipv4_address_no_regex?(str)}"
